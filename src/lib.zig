ae9863cd3d7a6c8708575fef1cfe29ad794f83cf
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2026-04-20 12:22:49 +0700

    refactor: relocate code
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- src/lib.zig
+++ src/lib.zig
@@ -1,32 +1,4 @@
-const std = @import("std");
-const json = std.json;
-
 test {
-    _ = @import("./graphql.zig");
+    _ = @import("./core/graphql.zig");
 }
 
-test "parse json" {
-    const json_data =
-        \\{
-        \\  "data": {
-        \\    "exec": "Hello"
-        \\  }
-        \\}
-    ;
-
-    const ExecResult = struct {
-        exec: []const u8,
-    };
-    _ = ExecResult;
-
-    const GraphQLResponse = struct {
-        data: json.Value,
-    };
-
-    var parsed = try json.parseFromSlice(GraphQLResponse, std.testing.allocator, json_data, .{ .ignore_unknown_fields = true });
-    defer parsed.deinit();
-
-    // const graphql_response: GraphQLResponse = parsed.value;
-    // const value = graphql_response.data;
-    // try std.testing.expectEqualStrings("Hello", value.string);
-}
